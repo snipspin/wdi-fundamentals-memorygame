c25909a491629b7a9f395a0eb1efbae91bfa2adb
Christian Spinney <photonenimpuls@gmail.com>
2019-12-08 23:51:01 -0800

    Added variables for each card plus a function to return a concatenated string

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+var cardOne = 'queen'
+var cardThree = 'queen';
+var cardTwo = 'king'
+var cardFour = 'king';
+
+flipped(cardTwo);
+
+function flipped(card) {
+	var flippedMsg = 'User flipped ';
+	console.log(flippedMsg + card);
+}
