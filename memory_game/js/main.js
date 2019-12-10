a30bcffe8f8f06bfd78bedc5d9e43b992023542d
Christian Spinney <photonenimpuls@gmail.com>
2019-12-09 20:44:48 -0800

    Moved the comparison logic into a function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,19 +8,24 @@ cardTwo = cards[2];
 cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
 
-if(cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
-	} 
-	else {
-		alert('Sorry, try again.');
-	} 
-}
+checkForMatch(cardsInPlay);
+
 
 flipped(cardOne);
 flipped(cardsInPlay[0]);
 flipped(cardsInPlay[1]);
 flipped(cards);
+
+function checkForMatch(cardsToCheckArray) {
+	if(cardsToCheckArray.length === 2) {
+		if(cardsToCheckArray[0] === cardsToCheckArray[1]) {
+			alert('You found a match!');
+		} 
+		else {
+			alert('Sorry, try again.');
+		} 
+	}
+}
 
 function flipped(card) {
 	var flippedMsg = 'User flipped ';
