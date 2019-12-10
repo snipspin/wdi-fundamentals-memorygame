a35086cd67a5b1fae111a3d11f0d09e5e9fa8a56
Christian Spinney <photonenimpuls@gmail.com>
2019-12-10 10:38:46 -0800

    Refactored functionality of flipping and matching cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,33 +1,26 @@
 const cards = ['queen', 'queen', 'king', 'king'];
 const cardsInPlay = [];
-let cardOne;
-let cardTwo;
 
-cardOne = cards[0];
-cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-checkForMatch(cardsInPlay);
-
-
-flipped(cardOne);
-flipped(cardsInPlay[0]);
-flipped(cardsInPlay[1]);
-flipped(cards);
-
-function checkForMatch(cardsToCheckArray) {
-	if(cardsToCheckArray.length === 2) {
-		if(cardsToCheckArray[0] === cardsToCheckArray[1]) {
+function checkForMatch() {
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
 			alert('You found a match!');
 		} 
 		else {
 			alert('Sorry, try again.');
 		} 
+		cardsInPlay.length=0;
 	}
 }
 
-function flipped(card) {
-	var flippedMsg = 'User flipped ';
-	console.log(flippedMsg + card);
+function flipCard(cardId) {
+	console.log(`User flipped ${cards[cardId]}`);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
+
+flipCard(3);
+flipCard(2);
