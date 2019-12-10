7c817e3eb4477f400c7eb3a21244b59376725f88
Christian Spinney <photonenimpuls@gmail.com>
2019-12-09 20:40:32 -0800

    Added a match comparison logic that alerts the user with its result

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -9,15 +9,18 @@ cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
 
 if(cardsInPlay.length === 2) {
-	
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} 
+	else {
+		alert('Sorry, try again.');
+	} 
 }
 
 flipped(cardOne);
 flipped(cardsInPlay[0]);
 flipped(cardsInPlay[1]);
 flipped(cards);
-
-
 
 function flipped(card) {
 	var flippedMsg = 'User flipped ';
