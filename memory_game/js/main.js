ed1f52ab21a90ab4e82ca6f33a0bc1f8e75c3f2b
Christian Spinney <photonenimpuls@gmail.com>
2019-12-11 17:52:59 -0800

    Added the necessary game logic to make the game playable

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,30 @@
+const back = "images/back.png";
 const cards = [
 	{
 		rank: "queen",
 		suit: "hearts",
-		cardImage: "images/queen-of-hearts.jpg"
+		cardImage: "images/queen-of-hearts.png"
 	},
 	{
 		rank: "queen",
 		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.jpg"
+		cardImage: "images/queen-of-diamonds.png"
 	},
 	{
 		rank: "king",
 		suit: "hearts",
-		cardImage: "images/king-of-hearts.jpg"
+		cardImage: "images/king-of-hearts.png"
 	},
 	{
 		rank: "king",
 		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.jpg"
+		cardImage: "images/king-of-diamonds.png"
 	}
 ];
 
 const cardsInPlay = [];
+
+const gameBoard = document.querySelector('#game-board');
 
 function checkForMatch() {
 	if(cardsInPlay.length === 2) {
@@ -35,15 +38,24 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
+function createBoard() {
+	for (var i = cards.length - 1; i >= 0; i--) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', back);
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		gameBoard.appendChild(cardElement);
+	}
+}
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log(`cardID: ${cardId}`);
 	console.log(`User flipped ${cards[cardId].rank}`);
 	console.log(`Imagepath: ${cards[cardId].cardImage} suit: ${cards[cardId].suit}`);
 	cardsInPlay.push(cards[cardId]);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
-
-flipCard(3);
-flipCard(2);
+createBoard();
